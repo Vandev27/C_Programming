756114b6a21efda007c0b130ee299166760d832f
Vandev27 <133748948+Vandev27@users.noreply.github.com>
2026-07-12 20:23:14 +0530

    Day 18 Complete

---
+++ Day18/p2.c
@@ -0,0 +1,31 @@
+//Use of malloc() function- Dynamic memory allocation for array
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int n,i;
+	int *ptr;
+	printf("Enter the number of elements\n");
+	scanf("%d",&n);
+	ptr = malloc(n*sizeof(int));
+	if(ptr==NULL)
+	{
+		printf("Error in memory allocation\n");
+		exit(0);//Function which is use to abnormaly
+	//	return;//Stop the programme
+	}
+	else
+	{
+		printf("Enter the elements of array\n");
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",(ptr+i));
+		}
+		printf("Display the elements of array\n");
+		for(i=0;i<n;i++)
+		{
+			printf("%d\t",*(ptr+i));
+		}
+		free(ptr);
+	}
+}
